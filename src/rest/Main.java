db41e967a4d23f75f117d65d948063bfe33cccdd
WaspGeekBrains <wasporc@mail.ru>
2020-09-19 13:12:52 +0300

    changeOneAndZero

--- src/rest/Main.java
+++ src/rest/Main.java
@@ -1,8 +1,25 @@
 package rest;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Задание 1: " + Arrays.toString(
+                changeOneAndZero(new int[]{1, 1, 0, 0, 1, 0, 1, 1, 0, 0})));
+    }
+
+    /**
+     * Задать целочисленный массив, состоящий из элементов 0 и 1.
+     * Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+     * С помощью цикла и условия заменить 0 на 1, 1 на 0;
+     * @param array целочисленный массив.
+     */
+    static int[] changeOneAndZero(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) array[i] = 1;
+            else array[i] = 0;
+        }
+        return array;
     }
 }
